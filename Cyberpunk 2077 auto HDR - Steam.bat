e44af475d6e7b8d46b52e2d9530c0ebad3dda03c
Antorell <25066224+Antorell@users.noreply.github.com>
2020-12-20 00:12:00 +0100

    Update Cyberpunk 2077 auto HDR - Steam.bat

--- Cyberpunk 2077 auto HDR - Steam.bat
+++ Cyberpunk 2077 auto HDR - Steam.bat
@@ -1,6 +1,8 @@
 @echo off
 cd /d "%~dp0"
-start "" "hdr_switch_tray.exe" && start "" "hdr_switch_tray.exe" hdr
+start "" "hdr_switch_tray.exe" 
+timeout /NOBREAK 1
+start "" "hdr_switch_tray.exe" hdr
 timeout /NOBREAK 3
 :: start "" explorer "steam://rungameid/1091500"
 start "" "C:\Program Files (x86)\Steam\Steam.exe" -applaunch 1091500
